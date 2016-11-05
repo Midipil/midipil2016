ea27981187c7b699da8958cc467aea64727123d7
Baptiste Moizard <baptiste-moizard@orange.fr>
2016-11-05 20:30:56 +0100

    Assets in monster scene

---
+++ Assets/Scripts/Monster/MonsterAnimationsController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MonsterAnimationsController : MonoBehaviour {
+
+	public Animator animator;
+
+	private float timeBeforeRoar = 7.0f;
+
+	// Use this for initialization
+	void Start () {
+		animator.SetFloat ("TimeBeforeRoar", timeBeforeRoar);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		animator.SetFloat ("TimeBeforeRoar", animator.GetFloat ("TimeBeforeRoar") - Time.deltaTime);
+		if (!animator.GetCurrentAnimatorStateInfo(0).IsName("Idle breathing")) {
+			animator.SetFloat ("TimeBeforeRoar", timeBeforeRoar);
+		}
+	}
+
+	public void TriggerAttack(){
+		animator.SetTrigger ("TriggerAttack");
+	}
+
+	public void TriggerDeath(){
+		animator.SetTrigger ("TriggerDeath");
+	}
+
+}
