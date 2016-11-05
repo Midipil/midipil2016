2bda280e8f93544e7478316fb1a671e9c8f7b389
Baptiste Moizard <baptiste-moizard@orange.fr>
2016-11-05 09:55:42 +0100

    Added scene manager

---
+++ Assets/Scripts/SceneManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class SceneManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	// Init scene and its content
+	protected abstract void InitScene();
+
+	// Get from GameManager the previous scene state (last time we entered this scene)
+	protected abstract void GetPreviousState();
+
+	// Start end sequence when scene goal is achieved
+	public abstract void SetEnd(bool win);
+
+}
