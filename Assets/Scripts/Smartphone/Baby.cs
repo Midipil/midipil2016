7b64aeb338fbc0ff30cbb6e25b71140f197ccf18
Joris HORMANCEY <j.hormancey@gmail.com>
2016-11-05 23:42:15 +0100

    Added fx for cradle in Smartphone scene

---
+++ Assets/Scripts/Smartphone/Baby.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Baby : MonoBehaviour 
+{
+	public GameObject fxSpawn;
+	public Transform anchorFx;
+
+	// Use this for initialization
+	void Start () 
+	{
+		Instantiate(fxSpawn, anchorFx.position, Quaternion.identity);
+	}
+}
