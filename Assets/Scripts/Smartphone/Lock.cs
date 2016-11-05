4c90c64604fe799c7ec92aa81358942a665d53e5
Joris HORMANCEY <j.hormancey@gmail.com>
2016-11-05 14:28:30 +0100

    Scene Smartphone first version

---
+++ Assets/Scripts/Smartphone/Lock.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Lock : MonoBehaviour 
+{
+	public Door door;
+
+	void OnSnap()
+	{
+		door.Open();
+	}
+}
---
+++ Assets/Scripts/Smartphone/Smartphone.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Smartphone : MonoBehaviour 
+{
+	public Image batteryImg;
+	public int batteryLifeSeconds;
+
+	public int batteryLife
+    {
+		get 
+		{
+		    if(batteryImg != null)
+		        return (int)(batteryImg.fillAmount * 100);	
+		    else
+		        return 0;	
+		}
+		set 
+		{
+	 	    if(batteryImg != null)
+		        batteryImg.fillAmount = value / 100f;	
+		} 
+    }
+
+    private float startTime;
+
+	void Start () 
+	{
+		batteryLife = 100;
+		startTime = Time.time;
+	}
+	
+	void Update () 
+	{
+		float currentTime = Time.time - startTime;
+		if(currentTime <= batteryLifeSeconds)
+		{
+			batteryLife = 100 - (int)((currentTime / batteryLifeSeconds) * 100);
+		}
+
+		if(batteryLife <= 0)
+		{
+
+		}
+	}
+}
