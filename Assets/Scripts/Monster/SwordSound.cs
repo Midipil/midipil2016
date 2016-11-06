1266adafbc7e72c794c09acd35d4292ce04e0704
Baptiste Moizard <baptiste-moizard@orange.fr>
2016-11-06 09:56:54 +0100

    Monster sounds

---
+++ Assets/Scripts/Monster/SwordSound.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof (Collider))]
+public class SwordSound : MonoBehaviour {
+
+	public AudioSource[] swordSounds;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider other) {
+		swordSounds [Random.Range (0, swordSounds.Length)].Play ();
+	}
+
+}
