1fb0dbd38ad965f0fad9e86b660c2f0dd58b4f2f
Baptiste Moizard <baptiste-moizard@orange.fr>
2016-11-06 03:27:09 +0100

    Added start menu scene

---
+++ Assets/Scripts/StartButton.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class StartButton : MonoBehaviour {
+
+	public float pressOffset = 0.01f;
+	Transform child;
+	Vector3 initPos;
+
+	// Use this for initialization
+	void Start () {
+
+		child = this.transform.GetChild (0);
+		initPos = child.localPosition;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider o) {
+		
+		child.localPosition = new Vector3 (initPos.x, initPos.y  -  pressOffset, initPos.z);
+
+		SceneManager.LoadScene ("Smartphone", LoadSceneMode.Single);
+
+	} 
+
+	void OnTriggerExit(Collider o) {
+
+		child.localPosition = initPos;
+
+	} 
+
+}
