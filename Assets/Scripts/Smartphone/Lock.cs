5e408eb081cea22b5e6e53ae6cfcd0db834d7aa6
Joris HORMANCEY <j.hormancey@gmail.com>
2016-11-06 10:15:23 +0100

    Fix smartphone & added ambient sounds

--- Assets/Scripts/Smartphone/Lock.cs
+++ Assets/Scripts/Smartphone/Lock.cs
@@ -4,9 +4,13 @@ using System.Collections;
 public class Lock : MonoBehaviour 
 {
 	public Door door;
+	public Transform anchor;
 
 	void OnSnap()
 	{
+		Transform key = transform.Find("Key");
+		if(key)
+			key.position = anchor.position;
 		door.Open();
 	}
 }
