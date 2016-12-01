5902aa2d3f361a6e24f1eb23f3fda73f1288064d
Baptiste Moizard <baptiste-moizard@orange.fr>
2016-12-01 19:57:52 +0100

    Improved cake detection in the hoven

---
+++ Assets/Scripts/Coffe/IsTheCakeInside.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Collider))]
+public class IsTheCakeInside : MonoBehaviour {
+
+    Hoven hoven = null;
+
+	// Use this for initialization
+	void Start () {
+        gameObject.GetComponent<Collider>().isTrigger = true;
+        hoven = FindObjectOfType<Hoven>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerStay(Collider other)
+    {
+        if (this.enabled && other.name == "Cake" && hoven != null)
+        {
+            hoven.cakePlaced = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (this.enabled && other.name == "Cake" && hoven != null)
+        {
+            hoven.cakePlaced = false;
+        }
+    }
+
+}
